64666b4b9e1680a347de783e138c3b2cb82f5bb2
Engineer-kim <ddang0103@naver.com>
2024-10-17 18:36:17 +0900

    Feature: 리액트에서의 라우팅 처리 하는 방법

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { createBrowserRouter ,createRoutesFromChildren,createRoutesFromElements,Route,RouterProvider } from "react-router-dom";
+import HomePage from "./pages/Home";
+import ProductPage from "./pages/Prodcut";
+
+// const routeDefintions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/"  element= {<HomePage/>} />
+//     <Route path="/products"  element= {<ProductPage/>} />
+//   </Route>
+// )
+
+const router = createBrowserRouter([
+  { path: '/', element: <HomePage/> },
+  { path: '/products', element: <ProductPage/>}
+]);
+
+// const rourter = createBrowserRouter(routeDefintions)
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router}/> ;
 }
 
 export default App;
